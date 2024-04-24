d6553e46e99d036541b88524f5b439b977ab9e29
Артем <@DESKTOP-HGILPRJ>
2024-04-24 17:22:00 +0300

    Добавьте файлы проекта.

---
+++ MarketplacePetProj/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace MarketplacePetProj.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string description { get; set; }
+        public int quantity { get; set; }
+        
+
+    }
+}
